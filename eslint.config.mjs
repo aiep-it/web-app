391f6153174c4087e88c014f7a3e8d5dac1ca03d
toanpq <atondz@github.com>
2025-06-29 13:36:25 +0700

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -85,67 +85,86 @@ export default defineConfig([globalIgnores([
 
     files: ["**/*.ts", "**/*.tsx"],
 
+    // rules: {
+    //     "no-console": "warn",
+    //     "react/prop-types": "off",
+    //     "react/jsx-uses-react": "off",
+    //     "react/react-in-jsx-scope": "off",
+    //     "react-hooks/exhaustive-deps": "off",
+    //     "jsx-a11y/click-events-have-key-events": "warn",
+    //     "jsx-a11y/interactive-supports-focus": "warn",
+    //     "prettier/prettier": "warn",
+    //     "no-unused-vars": "off",
+    //     "unused-imports/no-unused-vars": "off",
+    //     "unused-imports/no-unused-imports": "warn",
+
+    //     "@typescript-eslint/no-unused-vars": ["warn", {
+    //         args: "after-used",
+    //         ignoreRestSiblings: false,
+    //         argsIgnorePattern: "^_.*?$",
+    //     }],
+
+    //     "import/order": ["warn", {
+    //         groups: [
+    //             "type",
+    //             "builtin",
+    //             "object",
+    //             "external",
+    //             "internal",
+    //             "parent",
+    //             "sibling",
+    //             "index",
+    //         ],
+
+    //         pathGroups: [{
+    //             pattern: "~/**",
+    //             group: "external",
+    //             position: "after",
+    //         }],
+
+    //         "newlines-between": "always",
+    //     }],
+
+    //     "react/self-closing-comp": "warn",
+
+    //     "react/jsx-sort-props": ["warn", {
+    //         callbacksLast: true,
+    //         shorthandFirst: true,
+    //         noSortAlphabetically: false,
+    //         reservedFirst: true,
+    //     }],
+
+    //     "padding-line-between-statements": ["warn", {
+    //         blankLine: "always",
+    //         prev: "*",
+    //         next: "return",
+    //     }, {
+    //         blankLine: "always",
+    //         prev: ["const", "let", "var"],
+    //         next: "*",
+    //     }, {
+    //         blankLine: "any",
+    //         prev: ["const", "let", "var"],
+    //         next: ["const", "let", "var"],
+    //     }],
+    // },
+
+    //simple rule
+
     rules: {
-        "no-console": "warn",
-        "react/prop-types": "off",
-        "react/jsx-uses-react": "off",
-        "react/react-in-jsx-scope": "off",
-        "react-hooks/exhaustive-deps": "off",
-        "jsx-a11y/click-events-have-key-events": "warn",
-        "jsx-a11y/interactive-supports-focus": "warn",
-        "prettier/prettier": "warn",
-        "no-unused-vars": "off",
-        "unused-imports/no-unused-vars": "off",
-        "unused-imports/no-unused-imports": "warn",
-
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            args: "after-used",
-            ignoreRestSiblings: false,
-            argsIgnorePattern: "^_.*?$",
-        }],
-
-        "import/order": ["warn", {
-            groups: [
-                "type",
-                "builtin",
-                "object",
-                "external",
-                "internal",
-                "parent",
-                "sibling",
-                "index",
+        '@nx/enforce-module-boundaries': [
+          'error',
+          {
+            enforceBuildableLibDependency: true,
+            allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+            depConstraints: [
+              {
+                sourceTag: '*',
+                onlyDependOnLibsWithTags: ['*'],
+              },
             ],
-
-            pathGroups: [{
-                pattern: "~/**",
-                group: "external",
-                position: "after",
-            }],
-
-            "newlines-between": "always",
-        }],
-
-        "react/self-closing-comp": "warn",
-
-        "react/jsx-sort-props": ["warn", {
-            callbacksLast: true,
-            shorthandFirst: true,
-            noSortAlphabetically: false,
-            reservedFirst: true,
-        }],
-
-        "padding-line-between-statements": ["warn", {
-            blankLine: "always",
-            prev: "*",
-            next: "return",
-        }, {
-            blankLine: "always",
-            prev: ["const", "let", "var"],
-            next: "*",
-        }, {
-            blankLine: "any",
-            prev: ["const", "let", "var"],
-            next: ["const", "let", "var"],
-        }],
-    },
+          },
+        ],
+        '@typescript-eslint/ban-ts-comment': 'off'
+      },
 }]);
